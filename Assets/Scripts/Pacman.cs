8391709246791ab89f35e6dc89e55ae7c13a6f30
SteyvenAlive <carlos.arceo5903@alumnos.udg.mx>
2023-03-09 00:27:58 -0600

    first commit

---
+++ Assets/Scripts/Pacman.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+[RequireComponent(typeof(Movement))]
+public class Pacman : MonoBehaviour
+{
+    
+    public Movement movement { get; private set; }
+    private void Awake()
+    {
+        this.movement = GetComponent<Movement>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+            this.movement.setDirection(Vector2.up);
+        } 
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+            this.movement.setDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+            this.movement.setDirection(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            this.movement.setDirection(Vector2.right);
+        }
+
+        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Deg2Rad, Vector3.forward);
+    }
+}
